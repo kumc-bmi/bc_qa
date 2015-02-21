a8ec87850db0ab92fe59904e9f18552a21b38029
Dan Connolly <dconnolly@kumc.edu>
2015-02-20 19:39:11 -0600

    summary table seems to work

--- bc_qa_txform.R
+++ bc_qa_txform.R
@@ -84,7 +84,6 @@ factor.combine <- function(...) {
   factors <- list(...)
   x <- factors[[1]]
   for (ix in 2:length(factors)) {
-    print(length(x))
     cx <- as.character(x)
     cy <- as.character(factors[[ix]])
     cxy <- paste(cx, cy)
@@ -113,16 +112,16 @@ v.enc <- function(conn, var.path, var.name) {
   where v.concept_path = ?
   '
   per.enc <- dbGetPreparedQuery(conn, sql.summary, bind.data=data.frame(path=var.path))
-  per.enc$start_date <- as.POSIXct(per.enc$start_date)
+  # per.enc$start_date <- as.POSIXct(per.enc$start_date)
   
   names(per.enc)[3] <- var.name
   per.enc
 }
 
-show.issues <- function(tumor.site,
+show.issues <- function(tumor.site, var.excl,
                         threshold=50) {
-  for (ix in 1:length(var.exclusion)) {
-    v <- var.exclusion[ix]
+  for (ix in 1:nrow(var.excl)) {
+    v <- row.names(var.excl)[ix]
     x <- tumor.site[[v]]
     qty <- length(x)
     absent <- qty - length(na.omit(x))
@@ -131,4 +130,37 @@ show.issues <- function(tumor.site,
       print(paste0('Too many ', v, ' missing: ', pct, '%.'))
     }
   }
+}
+
+bc.exclusions <- function(conn.site,
+                          dx_path='\\i2b2\\naaccr\\SEER Site\\Breast\\') {
+  tumor.site <- v.enc(conn.site, dx_path, 'bc.dx')
+  tumor.site <- with.var(tumor.site, conn.site, var.excl['sex',]$concept_path, 'sex')
+  tumor.site <- with.var(tumor.site, conn.site, var.excl['seq.no',]$concept_path, 'seq.no')
+  tumor.site <- with.var(tumor.site, conn.site, var.excl['confirm',]$concept_path, 'confirm')
+  tumor.site <- with.var(tumor.site, conn.site, var.excl['morphology',]$concept_path, 'morphology')
+  
+  tumor.site <- with.var(
+    tumor.site, conn.site,
+    '\\i2b2\\naaccr\\S:11 Stage/Prognostic Factors\\3020 Derived SS2000\\', 'stage.ss')
+  tumor.site <- with.var(
+    tumor.site, conn.site,
+    '\\i2b2\\naaccr\\S:11 Stage/Prognostic Factors\\3430 Derived AJCC-7 Stage Grp\\', 'stage.ajcc')  
+  tumor.site$stage <- factor.combine(tumor.site$stage.ss, tumor.site$stage.ajcc)
+  
+  tumor.site <- with.var(
+    tumor.site, conn.site,
+    '\\i2b2\\naaccr\\S:4 Follow-up/Recurrence/Death\\1760 Vital Status\\',
+    'vital.tr')
+  
+  tumor.site <- with.var.pat(
+    tumor.site, conn.site,
+    '\\i2b2\\Demographics\\Vital Status\\Deceased\\', 'deceased.ehr')
+  tumor.site <- with.var.pat(
+    tumor.site, conn.site,
+    '\\i2b2\\Demographics\\Vital Status\\Deceased per SSA\\', 'deceased.ssa')
+  tumor.site$vital <- factor.combine(tumor.site$vital.tr,
+                                     tumor.site$deceased.ehr,
+                                     tumor.site$deceased.ssa)
+  tumor.site
 }
