33f55215ac65ff37f5520a06cb0e3348b3f56b18
Dan Connolly <dconnolly@kumc.edu>
2015-03-09 14:07:44 -0500

    migrate report_mail cli to docopt (untested!)

--- report_mail.py
+++ report_mail.py
@@ -1,3 +1,18 @@
+'''report_mail -- send reports to GPC sites by email
+
+Usage:
+  report_mail [options] BODY [<site>]...
+
+Options:
+  --readme=FILE      first attachment to send [default: README.html]
+  --terms-report=F   file explaining expected terms [default: bc_qa2.html]
+  --datadir=DIR      directory of data files to send [default: data-files]
+  --sender=ADDR      SMTP sender of messages [default: dconnolly@kumc.edu]
+  --cc=ADDR...       addresses to copy [default: dconnolly@kumc.edu,jhe@kumc.edu,tmcmahon@kumc.edu,e-chrischilles@uiowa.edu,bradley-mcdowell@uiowa.edu,tshireman@kumc.edu,vleonardo@kumc.edu]  # noqa
+  --site-email=FILE  file of site to email address mappings [default: bc_site_email.txt]  # noqa
+
+'''
+
 import csv
 import sys
 
@@ -7,36 +22,29 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE
 
+from docopt import docopt
 
-def main(argv,
-         report_mail='bc_site_email.txt',
-         readme='README.html',
-         terms_article='bc_qa2.html',
-         df='data-files',
-         sender='dconnolly@kumc.edu',
-         cc=['dconnolly@kumc.edu',
-             'jhe@kumc.edu',
-             'tmcmahon@kumc.edu',
-             'e-chrischilles@uiowa.edu',
-             'bradley-mcdowell@uiowa.edu',
-             'tshireman@kumc.edu',
-             'vleonardo@kumc.edu']):
-    body = argv[1]
+
+def main(argv):
+    cli = docopt(__doc__, argv[1:])
+    body = cli['BODY']
     cli_sites = argv[2:]
-    for item in csv.DictReader(open(report_mail)):
+    for item in csv.DictReader(open(cli['--site-email'])):
         site = item['site']
 
     if cli_sites and site not in cli_sites:
         continue
     if 'test' in body:
         cc = []
+    else:
+        cc = cli['--cc'].split(',')
 
     subject = 'Breast Cancer QA report for %s' % site
-    report = '%s/report-%s.html' % (df, site)
+    report = '%s/report-%s.html' % (cli['--datadir'], site)
     mbox = item['email']
-    send_mail(sender, [mbox] + cc, subject,
+    send_mail(cli['sender'], [mbox] + cc, subject,
               '%s,\n%s' % (item['name'], body),
-              files=[readme, terms_article, report])
+              files=[cli['--readme'], cli['--terms-report'], report])
 
 
 def send_mail(send_from, send_to, subject, text, files=None,
