2889b16a986afff561dbdb90601f54b39d8c9034
Dan Connolly <dconnolly@kumc.edu>
2016-09-21 14:28:53 -0500

    meds data dictionary prototype

--- bc2_dd.py
+++ bc2_dd.py
@@ -13,6 +13,14 @@ import pandas as pd
 dict(pandas=pd.__version__)
 
 
+# In[ ]:
+
+def file_access():
+    from pathlib import Path
+    return Path('.')
+cwd = file_access()
+
+
 # ## Babel DB Access
 
 # In[ ]:
@@ -32,14 +40,11 @@ babel_db.execute('select 1+1').fetchone()
 
 # ## Data Dictionary
 
-# ### i2b2 query
+# ### i2b2 query: BC Phase 2e of Dec 17, 2015
 
 # In[ ]:
 
-def file_access():
-    from pathlib import Path
-    return Path('.')
-cwd = file_access()
+bc295 = cwd / 'bc295_query_definition.xml'
 
 
 # In[ ]:
@@ -59,7 +64,7 @@ item_ont(r'\\i2b2_Medications\i2b2\Medications\RXAUI:3257')
 
 # In[ ]:
 
-with (cwd / 'bc295_query_definition.xml').open() as s:
+with bc295.open() as s:
     qdef = ET.parse(s).getroot()
 
 def e2d(e, keys):
@@ -85,17 +90,163 @@ qitem = pd.DataFrame([dict(e2d(panel, panel_cols) + e2d(item, item_cols))
 qitem['short_name'] = qitem.item_name.apply(strip_counts)
 qitem['ont'] = qitem.item_key.apply(item_ont)
 
-qitem[['panel_number', 'hlevel', 'ont', 'tooltip', 'short_name']]
+q_nice_cols = ['panel_number', 'hlevel', 'ont', 'tooltip', 'short_name']
+qitem[q_nice_cols]
 
 
 # In[ ]:
 
-qitem[~qitem.ont.isin(['naaccr', 'Diagnoses', 'Procedures'])][['panel_number', 'hlevel', 'ont', 'tooltip', 'short_name']]
+qitem[~qitem.ont.isin(['naaccr', 'Diagnoses', 'Procedures'])][q_nice_cols]
 
 
 # In[ ]:
 
 qitem.iloc[46]
+
+
+# #### Medications: Antineoplastics, Hormones
+
+# In[ ]:
+
+qitem[qitem.ont == 'Medications'][q_nice_cols]
+
+
+# In[ ]:
+
+def gpc_std(item_key):
+    return '\\GPC\\' + item_key.split('\\', 4)[-1]
+
+
+# In[ ]:
+
+med_term_paths = ', '.join("'%s'" % p
+                           for p in qitem[qitem.ont == 'Medications'].item_key.apply(gpc_std))
+print med_term_paths
+
+
+# In[ ]:
+
+pd.read_sql(r'''
+select *
+from i2b2metadata.gpc_terms
+where c_fullname in ({paths})
+limit 100
+'''.format(paths=med_term_paths), babel_db)
+
+
+# In[ ]:
+
+# Note %s have to be doubled in sqlalchemy API
+sql = r'''
+with va_top as (
+  select *
+  from i2b2metadata.gpc_terms
+  where c_fullname in ({paths})
+)
+, va_class as (
+  select sub.c_hlevel, sub.c_basecode, sub.c_name, sub.c_fullname
+  from i2b2metadata.gpc_terms sub
+  join va_top on sub.c_fullname like (va_top.c_fullname || '%%') escape '@'
+  where sub.c_name like '[%%]%%'
+)
+select * from va_class
+order by c_fullname
+limit 100
+'''.format(paths=med_term_paths)
+med_va_class = pd.read_sql(sql, babel_db)
+med_va_class['code'] = med_va_class.c_name.apply(lambda s: s.split('] ')[0][1:])
+med_va_class.set_index('code')[['c_name']]
+
+
+# In[ ]:
+
+# Note %s have to be doubled in sqlalchemy API
+sql = r'''
+with va_top as (
+  select *
+  from i2b2metadata.gpc_terms
+  where c_fullname in ({paths})
+)
+, va_class as (
+  select sub.c_hlevel, sub.c_basecode, sub.c_name, sub.c_fullname
+  from i2b2metadata.gpc_terms sub
+  join va_top on sub.c_fullname like (va_top.c_fullname || '%%') escape '@'
+  where sub.c_name like '[%%]%%'
+)
+, rx as (
+  select sub.c_hlevel, sub.c_basecode, sub.c_name
+       , substr(va_class.c_name, 2, 5) va_class_code
+       , sub.c_fullname
+  from i2b2metadata.gpc_terms sub
+  join va_class
+    on sub.c_fullname like (va_class.c_fullname || '%%') escape '@'
+   and sub.c_hlevel = va_class.c_hlevel + 1
+  where sub.c_name not like '[%%]%%'
+)
+select distinct * from rx
+order by va_class_code, c_basecode
+'''.format(paths=med_term_paths)
+rx = pd.read_sql(sql, babel_db)
+print 'distinct drugs:', len(rx)
+print 'hlevels:', rx.c_hlevel.unique()
+rx['rxcui'] = rx.c_basecode.apply(lambda s: s.split(':')[1])
+
+
+
+
+# #### Unique Drugs: eliminating polyhierarchy
+
+# In[ ]:
+
+len(rx.rxcui), len(rx.rxcui.unique())
+
+
+# In[ ]:
+
+rx.sort_values('rxcui', inplace=True)
+dup_cuis = rx[rx.rxcui.duplicated()].rxcui
+rx[rx.rxcui.isin(dup_cuis)]
+
+
+# In[ ]:
+
+#rx1 = pd.DataFrame(dict(va_class_code=rx.groupby('rxcui').va_class_code.min()))
+rx1 = rx.groupby('rxcui')[['va_class_code', 'c_name']].min()
+print len(rx1)
+rx1.head()
+
+
+# In[ ]:
+
+rx1['label'] = ['[{vc}] RXCUI:{cui} {drug}'.format(vc=drug.va_class_code, cui=rxcui, drug=drug.c_name)
+                for (rxcui, drug) in rx1.iterrows()]
+rx1.sort_values(['va_class_code', 'c_name'], inplace=True)
+rx1.head(20)[['va_class_code', 'c_name', 'label']]
+
+
+# ## Data Dictionary for REDCap
+
+# In[ ]:
+
+ddict_prototype = pd.read_csv((cwd / 'redcap_ddict_prototype.csv').open()).set_index(u'Variable / Field Name')
+ddict_prototype.columns
+
+
+# In[ ]:
+
+def choices(series):
+    return ' | '.join('{code}, {label}'.format(code=code, label=label)
+               for (code, label) in series.iteritems())
+
+
+# In[ ]:
+
+med_ddict = ddict_prototype.copy()
+med_ddict.set_value('va_class', u'Choices, Calculations, OR Slider Labels', choices(med_va_class.set_index('code').c_name))
+med_ddict.set_value('rxcui', u'Choices, Calculations, OR Slider Labels', choices(rx1.label))
+
+med_ddict.to_csv('med_exposure_ddict.csv')
+med_ddict
 
 
 # ### Data Management Spreadsheet Dec 2015
